159b59a893278f08ac571538c122baab80486f41
Luis Axel <axelcruz40@aragon.unam.mx>
2024-07-02 21:44:04 -0600

    start

---
+++ src/pelicula/Pelicula.java
@@ -0,0 +1,22 @@
+package pelicula;
+/**
+ *
+ * @author axeli
+ */
+public class Pelicula {
+    
+    private Sring titulo, genero, creador, year, duracion,visto;
+    
+    public Pelicula(){}
+    
+    public Pelicula(String titulo,String creador){
+        this.titulo = titulo;
+        this.creador = creador;
+    }
+        
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
